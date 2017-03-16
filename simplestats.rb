e4d4717f54cb9b669b0858e93a46c469a52223c2
Damon Buckwalter <damon@cryptomeme.com>
2017-03-16 09:50:08 -0700

    Updated the service to return JSON for each endpoint, tests pass

--- simplestats.rb
+++ simplestats.rb
@@ -1,9 +1,32 @@
 ## Implementation of the Simple Stats API REST server endpoints
 
+require "json"
 require "sinatra"
 
 
 API_VERSION = "1"
+
+def parse_info(file)
+	# parse the /proc/cpuinfo to JSON
+	begin
+		f = open(file,'r')
+
+		h = {}
+
+		f.readlines.each do |l|
+			l.chomp!
+			# the regex below breaks the {mem,cpu}info into k/v
+			e = l.split(/\t*:\s+/)
+
+			# if our split succeeded, store the k/v
+			h.store e[0], e[1] if e.length == 2
+		end
+	ensure
+		f.close
+	end
+
+	return h
+end
 
 
 get "/ping" do
@@ -16,43 +39,41 @@ end
 
 get "/cpuinfo" do
 	content_type "application/json"
-	
-	# parse the /proc/cpuinfo to JSON
-	begin
-		f = open('/proc/cpuinfo','r')
-	rescue Exception
-		status 500
-		halt
-	end
 
+	# parse the /proc/cpuinfo to JSON
 	# return result
+	return JSON.generate parse_info('/proc/cpuinfo')
 end
 
 get "/meminfo" do
 	content_type "application/json"
 
 	# parse the /proc/meminfo to JSON
-	begin
-		f = open('/proc/meminfo','r')
-	rescue Exception
-		status 500
-		halt
-	end
 
 	# return result
+	return JSON.generate parse_info('/proc/meminfo')
 end
 
 
 get "/uptime" do
 	content_type "application/json"
 
+	h = {}
+	
 	# parse the /proc/uptime to JSON
 	begin
 		f = open('/proc/uptime','r')
-	rescue Exception
-		status 500
-		halt
+
+		t = f.readline.chomp.split(/\s+/)
+
+		if t.length == 2
+			h['Uptime (seconds)'] = t[0]
+			h['Idle time (seconds)'] = t[1]
+		end
+	ensure
+		f.close
 	end
 
 	# return result
+	return JSON.generate h
 end
